08a762b30f4f53add0f849d62603713d362d6e67
yz <yunzehhht@163.com>
2019-05-08 09:47:42 +0800

    删除dist

--- dist/js/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-console.log("111");
-new Promise(function (resolve, reject) {
-  setTimeout(function () {
-    resolve('ok');
-  }, 1000);
-});
-console.log(11); //
--- dist/js/index2.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-
-console.log(11);
-var str = '你好';
